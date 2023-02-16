b9d6d1d13c18b478dfb44630a231a83e72398802
trivedikushal <44478572+trivedikushal@users.noreply.github.com>
2023-02-16 14:51:52 -0500

    Delete InterviewCalender directory

--- InterviewCalender/src/main/java/com/rest/springboot/demo/repository/InterviewDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rest.springboot.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rest.springboot.demo.entity.InterviewDetails;
-
-public interface InterviewDetailsRepository extends JpaRepository<InterviewDetails, Integer> {
-
-}
--- InterviewCalender/src/main/java/com/rest/springboot/demo/repository/JobTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rest.springboot.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rest.springboot.demo.entity.JobType;
-
-public interface JobTypeRepository extends JpaRepository<JobType, Integer> {
-
-}
